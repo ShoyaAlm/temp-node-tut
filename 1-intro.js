9990ff5d08209693e3a8132e84864167a53c04d2
Shoya <shoyaalimi@gmail.com>
2021-08-21 17:00:26 +0430

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+const amount = 12;
+
+if(amount < 10){
+    console.log('small num');
+}
+else{
+    console.log('large num');
+}
+
+console.log(`hey its my first node app!!!`);
+
+
---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+
+
+
+console.log(__filename);
+setInterval(() => {
+    console.log('Hello World');
+}, 1000);
